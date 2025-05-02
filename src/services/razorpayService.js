2b41c94652df5884c2418bc145388f0564c04ef9
usaif1 <usaif1311@gmail.com>
2025-05-02 16:39:36 +0530

    cleanup

--- src/services/razorpayService.js
+++ src/services/razorpayService.js
@@ -1,7 +1,7 @@
 // services/easebuzzService.js
 const axios = require("axios");
 const { v4: uuidv4 } = require("uuid");
-const { generateEasebuzzHash } = require("../utils/generateHash");
+// const { generateEasebuzzHash } = require("../utils/generateHash");
 const { URLSearchParams } = require("url");
 const Razorpay = require("razorpay");
 
@@ -26,46 +26,6 @@ const initiatePayment = async (req, res) => {
     console.error("Easebuzz Error:", error);
     res.status(500).send("Payment failed");
   }
-
-  //   const txnId = uuidv4().toString();
-  //   const productinfo = "walletrecharge";
-
-  //   const encodedParams = new URLSearchParams({
-  //     key: process.env.MERCHANT_KEY,
-  //     txnid: txnId,
-  //     amount,
-  //     productinfo,
-  //     firstname,
-  //     phone,
-  //     email,
-  //     surl: "http://localhost:3000",
-  //     furl: "http://localhost:3000",
-  //     hash: generateEasebuzzHash(
-  //       {
-  //         key: process.env.MERCHANT_KEY,
-  //         txnid: txnId,
-  //         amount,
-  //         productinfo,
-  //         firstname,
-  //         email,
-  //       },
-  //       process.env.MERCHANT_SALT
-  //     ),
-  //   });
-
-  //   try {
-  //     const { data } = await axios.post(
-  //       process.env.EASEBUZZ_ENDPOINT,
-  //       encodedParams,
-  //       {
-  //         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //       }
-  //     );
-  //     res.send(data);
-  //   } catch (err) {
-  //   console.error("Easebuzz Error:", err.message);
-  //   res.status(500).send("Payment failed");
-  //   }
 };
 
 module.exports = { initiatePayment };
