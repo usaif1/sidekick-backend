e7a9d235f69d0ad66d89160dc9f862f7dfa8340b
Saif Ullah <saif.ullah@fuelbuddy.in>
2025-08-26 20:22:55 +0530

    fixes to enc key

--- src/services/solveService.js
+++ src/services/solveService.js
@@ -4,17 +4,11 @@ const {
 } = require("../utils/solveUtils");
 
 async function processSolveRequest(token, encKey) {
-  const PRE_SHARED_KEY = encKey;
-
-  if (!PRE_SHARED_KEY) {
-    throw new Error("PRE_SHARED_KEY environment variable is not set");
-  }
-
   if (!token) {
     throw new Error("Token is required");
   }
 
-  const hash = generateSolveHash(token, PRE_SHARED_KEY);
+  const hash = generateSolveHash(token, encKey);
   const commandBuffer = buildSolveCommandBuffer(hash);
 
   return { command: commandBuffer };
